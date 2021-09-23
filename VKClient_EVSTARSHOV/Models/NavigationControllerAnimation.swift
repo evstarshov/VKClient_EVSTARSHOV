35b3d5cf00ad2cae73c8f0d0b7b161fd3de7fe71
EugeneStarshov <yeenzo@yandex.ru>
2021-09-23 15:25:30 +0300

    updated pushanimation

--- VKClient_EVSTARSHOV/Models/NavigationControllerAnimation.swift
+++ VKClient_EVSTARSHOV/Models/NavigationControllerAnimation.swift
@@ -8,44 +8,59 @@
 import UIKit
 
 final class PushAnimation: NSObject, UIViewControllerAnimatedTransitioning {
-    private let animationTime = 0.5
+    private let animateTime = 0.5
     
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        animationTime
+        animateTime
     }
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         guard
-        let source = transitionContext.viewController(forKey: .from),
-        let destination = transitionContext.viewController(forKey: .to)
+            let source = transitionContext.viewController(forKey: .from),
+            let destination = transitionContext.viewController(forKey: .to)
         else { return }
         
         transitionContext.containerView.addSubview(destination.view)
-        
         destination.view.frame = transitionContext.containerView.frame
-        destination.view.transform = CGAffineTransform(translationX: source.view.bounds.width, y: 0)
+        destination.view.transform = CGAffineTransform(
+            translationX: source.view.bounds.width,
+            y: 0)
         
-        UIView.animateKeyframes(withDuration: animationTime, delay: 0.0, options: .calculationModePaced) {
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.75) {
-                let translation = CGAffineTransform(translationX: -250.0, y: 0.0)
-                let scale = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        UIView.animateKeyframes(
+            withDuration: animateTime,
+            delay: 0.0,
+            options: .calculationModePaced) {
+            UIView.addKeyframe(
+                withRelativeStartTime: 0,
+                relativeDuration: 0.75) {
+                let translation = CGAffineTransform(
+                    translationX: -250.0,
+                    y: 0.0)
+                let scale = CGAffineTransform(
+                    scaleX: 0.8,
+                    y: 0.8)
                 source.view.transform = translation.concatenating(scale)
             }
-            
-            UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.4) {
-                let translation = CGAffineTransform(translationX: source.view.bounds.width / 2, y: 0.0)
-                let scale = CGAffineTransform(scaleX: 1.2, y: 1.2)
-                destination.view.transform = translation.concatenating(scale)
-            }
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.4) {
+                    let translation = CGAffineTransform(
+                        translationX: source.view.bounds.width / 2,
+                        y: 0.0)
+                    let scale = CGAffineTransform(
+                        scaleX: 1.2,
+                        y: 1.2)
+                    destination.view.transform = translation.concatenating(scale)
+                }
             
             UIView.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.4) {
                 destination.view.transform = .identity
             }
-        } completion: { isComplited in
-            if isComplited && !transitionContext.transitionWasCancelled {
+        } completion: { isCompleted in
+            if isCompleted && !transitionContext.transitionWasCancelled {
                 source.view.transform = .identity
             }
-            transitionContext.completeTransition(isComplited && !transitionContext.transitionWasCancelled)
+            
+            transitionContext.completeTransition(isCompleted && !transitionContext.transitionWasCancelled)
         }
     }
 }
